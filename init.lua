a989bebbae7a5be88152866eed82d5dc423da154
Leonid Riznyk <leonid.riznyk@tokyotechies.com>
2024-08-09 12:26:39 +0900

    add oil nvim and fix completions

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
 require("ttriznykleo")
+
+local function safe_replace_commands()
+	local commands = {
+		"%s/Room/FacilityLocation/g",
+		"%s/room/facilityLocation/g",
+	}
+
+	for _, cmd in ipairs(commands) do
+		vim.cmd("silent! " .. cmd)
+	end
+end
+
+-- Create a Neovim command to call the function
+vim.api.nvim_create_user_command("SafeReplace", safe_replace_commands, {})
--- lua/ttriznykleo/lazy.lua
+++ lua/ttriznykleo/lazy.lua
@@ -67,6 +67,13 @@ local plugins = {
 	},
 	"tpope/vim-commentary",
 	"tpope/vim-surround",
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
 }
 
 local opts = {}
