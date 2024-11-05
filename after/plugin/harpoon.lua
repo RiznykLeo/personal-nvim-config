5d64ca73b8979bbfc11190a938149fc3d62abba5
Leonid Riznyk <leonid.riznyk@tokyotechies.com>
2024-11-05 15:09:12 +0900

    add harpoon key for specific OS

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,14 +1,20 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
+local OsHarpoonKey = "C"
+
 vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<" .. OsHarpoonKey .. "-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<A-h>", function() ui.nav_file(1) end)
-
-vim.keymap.set("n", "<A-t>", function() ui.nav_file(2) end)
-
-vim.keymap.set("n", "<A-n>", function() ui.nav_file(3) end)
-
-vim.keymap.set("n", "<A-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<" .. OsHarpoonKey .. "-h>", function()
+	ui.nav_file(1)
+end)
+vim.keymap.set("n", "<" .. OsHarpoonKey .. "-t>", function()
+	ui.nav_file(2)
+end)
+vim.keymap.set("n", "<" .. OsHarpoonKey .. "-n>", function()
+	ui.nav_file(3)
+end)
+vim.keymap.set("n", "<" .. OsHarpoonKey .. "-s>", function()
+	ui.nav_file(4)
+end)
