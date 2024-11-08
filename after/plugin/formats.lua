9d479dbf2501ac5d3d5bec48892492c8d1477b5d
Leonid Riznyk <leonid.riznyk@tokyotechies.com>
2024-11-08 17:41:14 +0900

    update formatter

--- after/plugin/formats.lua
+++ after/plugin/formats.lua
@@ -1,30 +1,30 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "eslint_d", "prettierd" },
-    typescript = { "eslint_d", "prettierd" },
-    javascriptreact = { "prettierd" },
-    typescriptreact = { "prettierd" },
-    css = { "prettierd" },
-    html = { "prettierd" },
-    json = { "prettierd" },
-    yaml = { "yamlfix" },
-    markdown = { "prettierd" },
-    go = { "gofmt", "goimports-reviser", "golines" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "yamlfix" },
+		markdown = { "prettier" },
+		go = { "gofmt", "goimports-reviser", "golines" },
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
 require("conform").setup({
-  format_after_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_before_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
